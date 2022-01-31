82cf17615ad2732e2a13d531757e7c33e8122631
Atom1116 <hiro_hika_1116@icloud.com>
2022-01-31 23:34:46 +0900

    gunicornのインターフェース実装

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,42 @@
 
 # 3. レスポンスボディとしてバイト文字列をyieldするiterableなオブジェクトを返す。
 
+import os
+from static_middleware import StaticMiddleware
+from response import Response, TemplateResponse
+from wsgiref.simple_server import make_server
+from app import App
+
+
 def app(env, start_response):
     start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
     print('Hello, World!!!')
     return [b'Hello World Hikaru']
+
+
+def create_app():
+    app = App()
+
+    BASE_DIR = os.path.dirname(__name__)
+    STATIC_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+    @app.route('^/$', 'GET')
+    def hello(request):
+        return Response('Hello World')
+
+    @app.route('^/user/$', 'POST')
+    def create_user(request):
+        return Response('User Created', status=201)
+
+    @app.route('^/user/$', 'GET')
+    def users(request):
+        users = ['user%s' % i for i in range(10)]
+        return TemplateResponse('users.html', title='User List', users=users)
+
+    @app.route('^/user/(?P<name>\w+)$', 'GET')
+    def user_detail(request, name):
+        return Response('Hello {name}'.format(name=name))
+
+    app = StaticMiddleware(app, static_root='static', static_dirs=STATIC_DIRS)
+
+    return app
