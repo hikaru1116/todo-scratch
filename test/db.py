b65330f960c38b3be2f7b36a0b1cfc5af60e0f4a
Atom1116 <hirohika1116@gmail.com>
2022-04-24 17:17:56 +0900

    python環境ファイル編集（DBライブラリ変更）

--- test/db.py
+++ test/db.py
@@ -1,4 +1,4 @@
-import pymysql.cursors
+import mysql.connector as mysql
 import unittest
 import sys
 # importモジュールのディレクトリを設定
@@ -12,30 +12,50 @@ class TestTemplate(unittest.TestCase):
         unittest (_type_): ユニットテスト
     """
 
+    db_config = {
+        'host': '192.168.64.3',
+        'port': '3306',
+        'user': 'admin',
+        'password': 'P@ssw0rd',
+        'database': 'todo_scratch'
+    }
+
     def test_class_loader(self):
-        connection = pymysql.connect(host='192.168.64.3',
-                                     user='admin',
-                                     password='P@ssw0rd',
-                                     database='todo_scratch',
-                                     cursorclass=pymysql.cursors.DictCursor)
-        with connection:
-            # with connection.cursor() as cursor:
-            #     # Create a new record
-            #     sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-            #     cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+        query_select = "select * from todo_scratch.`user`"
+        conn = mysql.connect(**self.db_config)
+        cur = conn.cursor()
+        cur.execute(query_select)
 
-            # # connection is not autocommit by default. So you must commit to save
-            # # your changes.
-            # connection.commit()
+        rows = cur.fetchall()
+        print(rows)
+        print(type(rows), type(rows[0]))
+        cur.close()
+        conn.close()
+        self.assertEqual(rows[0][1], 'hikaru')
 
-            with connection.cursor() as cursor:
-                # Read a single record
-                sql = "SELECT * FROM `user` WHERE `id`=%s"
-                cursor.execute(sql, (1,))
-                result = cursor.fetchone()
-                print(result)
+        query_insert = "insert into user(name, age, content) VALUES (%s,%s,%s)"
+        conn = mysql.connect(**self.db_config)
 
-        self.assertEqual('hikaru', result.get('name'))
+        # 自動コミット制御
+        conn.autocommit = False
+        cur = conn.cursor()
+        try:
+            cur.execute(query_insert, ['test7', '20', 'テストデータを挿入します'])
+            conn.commit()
+        except Exception as e:
+            print(e.args)
+            # ロールバック
+            conn.rollback()
+            cur.close()
+            conn.close()
+            raise Exception
+
+        cur.execute(query_select)
+        rows = cur.fetchall()
+        print('insert row is', rows[-1])
+        self.assertEqual(rows[-1][1], 'test7')
+        cur.close()
+        conn.close()
 
 
 if __name__ == '__main__':
