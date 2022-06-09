244947503c20db8c6fcc45b3045229d9c86ec129
Atom1116 <hirohika1116@gmail.com>
2022-06-09 23:14:19 +0900

    settings.pyへコメント追加

--- todo_scratch/bk_app/settings.py
+++ todo_scratch/bk_app/settings.py
@@ -1,12 +1,16 @@
-
-APP_PATH = 'todo_scratch.bk_app'
-
-URLS_PATH = 'urls'
-
+# デバックモード設定。
 IS_DEBUG = True
 
-DB_TYPE = "mysql"
+# アプリケーションプロジェクト設定(必須)
+# アプリケーションプロジェクトのパス設定
+APP_PATH = 'todo_scratch.bk_app'
+# urlルーティングファイルのファイル名
+URLS_PATH = 'urls'
 
+# DB設定 (DB操作する場合は、必須)
+# 操作するDBの種類 設定値:"mysql",（他DBは順次対応)
+DB_TYPE = "mysql"
+DB_NAME = 'todo_scratch'
 DB_CONFIG = {
     'host': '192.168.64.3',
     'port': '3306',
@@ -15,20 +19,23 @@ DB_CONFIG = {
     'database': 'todo_scratch'
 }
 
+# リソースアクセスを許可するオリジン設定
 ACCESS_ALLOW_ORIGIN = ["http://localhost:3000"]
 
-DB_NAME = 'todo_scratch'
-
+# ミドルウェア設定
+# ミドルウェア処理を実行するミドルウェアクラスを追加します。
 MIDDLEWARES = [
     "atom_bk_frame.middleware.http_log_middleware.HttpLogMiddleware",
     "atom_bk_frame.middleware.cors_middleware.CorsMiddleware",
     "atom_bk_frame.middleware.session_middleware.SessionMiddleware"
 ]
 
+# 認証・認可機能
+# 認証ユーザエンティティ設定（認証ユーザとなるテーブルを定義したエンティティを設定）
 AUTH_USER_ENTITY = "todo_scratch.bk_app.entities.user_entity.UserEntity"
-
+# 認証キー
 SECRET_KEY = "xxxxx"
-
-IS_TWO_STEP_VERFICATION_BY_EMAIL = False
-
+# セッションの有効期限
 SESSION_EXPIRED_DAYS = 31
+# 二段階認証の設定（未実装）
+IS_TWO_STEP_VERFICATION_BY_EMAIL = False
